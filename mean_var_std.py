5baf04b261dce226f05e6ab8dd074c77a16785d5
hcasellato <henriquecasellato@hotmail.com>
2022-02-02 14:02:25 -0300

    first code

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,2 +1,8 @@
-def calculate():
-    pass
+import numpy as np
+
+def calculate(matrix):
+    list = dict(mean = [])
+    matrix = np.reshape(matrix, (3, 3))
+    return matrix
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
