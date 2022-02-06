5617978b39865bdb3c5c5d6121ac1170240dc07f
hcasellato <henriquecasellato@hotmail.com>
2022-02-05 21:14:12 -0300

    final mean_var_std version :D

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,24 @@
 import numpy as np
 
 def calculate(matrix):
-    list = dict(mean = [])
-    matrix = np.reshape(matrix, (3, 3))
-    return matrix
+    if len(matrix) < 9:
+        raise ValueError ("List must contain nine numbers.")
+    np.array(matrix)
+    calc = {'mean':[], 'variance':[], 'standard deviation':[], 'max':[], 'min':[], 'sum':[]}
+    new_matrix = np.reshape(matrix, (3, 3))
+    for x in range(2):
+        calc['mean'].append(list(np.mean(new_matrix, axis=x)))
+        calc['variance'].append(list(np.var(new_matrix, axis=x)))
+        calc['standard deviation'].append(list(np.std(new_matrix, axis=x)))
+        calc['max'].append(list(np.max(new_matrix, axis=x)))
+        calc['min'].append(list(np.min(new_matrix, axis=x)))
+        calc['sum'].append(list(np.sum(new_matrix, axis=x)))
+    calc['mean'].append(np.mean(new_matrix))
+    calc['variance'].append(np.var(new_matrix))
+    calc['standard deviation'].append(np.std(new_matrix))
+    calc['max'].append(np.max(new_matrix))
+    calc['min'].append(np.min(new_matrix))
+    calc['sum'].append(np.sum(new_matrix))
+    return calc
 
-print(calculate([0,1,2,3,4,5,6,7,8]))
+# Ran 3 tests in 0.004s :D
